427f895f6953000addb9ecda09ac6a0e1ed4b98a
Sanjo <sanjoneyyan@gmail.com>
2025-02-05 15:13:07 +0530

    a

--- back-end/server.js
+++ back-end/server.js
@@ -1,13 +1,16 @@
+require('dotenv').config
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connection_string = process.env.MONGO_CONNECTION_STRING
+console.log(connection_string)
 app.use(cors());
 app.use(express.json());
 
-// mongoose.connect('mongodb://localhost/mern-stack-db', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(connection_string, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`); 
