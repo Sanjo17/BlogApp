b5e72d28f4342c0e8f13155c361ea92f048941e9
Sanjo <sanjoneyyan@gmail.com>
2025-02-05 15:43:21 +0530

    error solve

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,11 @@
-require('dotenv').config
+require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const connection_string = process.env.MONGO_CONNECTION_STRING
+const connection_string = process.env.CONNECTION_STRING
 app.use(cors());
 app.use(express.json());
 
