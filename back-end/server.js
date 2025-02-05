28b997f1c8fa2fb72f7194df6eaaa6bfec08d44c
Sanjo <sanjoneyyan@gmail.com>
2025-02-05 15:24:59 +0530

    k

--- back-end/server.js
+++ back-end/server.js
@@ -6,11 +6,10 @@ const cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 5000;
 const connection_string = process.env.MONGO_CONNECTION_STRING
-console.log(connection_string)
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(connection_string, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(connection_string);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`); 
