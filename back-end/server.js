f70d9d2a5bf3b5dd1e0fb9e98f2456a9cea821c8
Sanjo <sanjoneyyan@gmail.com>
2025-02-07 21:45:15 +0530

    checking the cors

--- back-end/server.js
+++ back-end/server.js
@@ -8,13 +8,7 @@ const blogRoutes = require('./routes/blogRoute');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const connection_string = process.env.CONNECTION_STRING
-app.use(cors(
-  {
-      // Change this to your React frontend URL for security
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization"
-  }
-));
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(connection_string).then(() => console.log('MongoDB Connected'))
