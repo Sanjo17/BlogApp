68cb6479305ef367ee6039c8e08cfdaeb06be04e
Sanjo <sanjoneyyan@gmail.com>
2025-02-07 21:53:33 +0530

    changed cors settings

--- back-end/server.js
+++ back-end/server.js
@@ -8,7 +8,14 @@ const blogRoutes = require('./routes/blogRoute');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const connection_string = process.env.CONNECTION_STRING
-app.use(cors());
+app.use(cors(
+  {
+    origin:["*"], 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+));
+
 app.use(express.json());
 
 mongoose.connect(connection_string).then(() => console.log('MongoDB Connected'))
